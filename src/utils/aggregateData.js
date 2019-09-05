a4ed6f7f7e0d7aedf0a7847121995a6556269630
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-09-05 11:11:40 +0300

    add utils

---
+++ src/utils/aggregateData.js
@@ -0,0 +1,21 @@
+const aggregateFunc = {
+  sum: data => data.reduce((a, b) => a + b.y, 0),
+  max: data => data.reduce((a, b) => (a > b.y ? a : b.y), 0),
+  min: data => data.reduce((a, b) => (a < b.y ? a : b.y), 0),
+  avg: data => data.reduce((a, b) => a + b.y, 0) / data.length,
+  last: data => data[-1],
+  first: data => data[0]
+};
+
+export default function aggregateData(func, data) {
+  const reduced = {};
+  const uniqueX = [...new Set(data.map(d => d.x))];
+  uniqueX.forEach(x => {
+    reduced[x] = {
+      x,
+      y: aggregateFunc[func](data.filter(d => d.x === x))
+    };
+  });
+
+  return Object.values(reduced);
+}
