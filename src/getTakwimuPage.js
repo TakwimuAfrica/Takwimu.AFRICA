8d90021c6d89524879de934309d40f4e0901cdcd
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-10-08 12:33:57 +0300

    add fetch function for wordpress pages

--- src/getTakwimuPage.js
+++ src/getTakwimuPage.js
@@ -14,3 +14,17 @@ export default async function getTakwimuPage(type) {
 
   return config;
 }
+
+export async function getSitePage(slug) {
+  console.time('download');
+  const res = await fetch(
+    `https://takwimutech.wpengine.com/wp-json/wp/v2/pages?slug=${slug}`
+  );
+  const data = await res.json();
+
+  console.timeEnd('download');
+
+  Object.assign(config.page, data[0].acf);
+
+  return config;
+}
