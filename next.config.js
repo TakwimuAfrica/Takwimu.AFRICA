c4dfff9696007ffe3da936483fc85857b048aa9b
Clemence Kyara <clemence@codeforafrica.org>
2019-11-20 16:45:28 +0300

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
 const withCSS = require('@zeit/next-css');
 const withTM = require('next-transpile-modules');
 const withImages = require('next-images');
 
-module.exports = withCSS(
-  withImages(
-    withTM({
-      transpileModules: ['@codeforafrica/hurumap-ui'],
-      target: 'serverless'
-    })
+module.exports = withBundleAnalyzer(
+  withCSS(
+    withImages(
+      withTM({
+        transpileModules: ['@codeforafrica/hurumap-ui'],
+        target: 'serverless'
+      })
+    )
   )
 );
