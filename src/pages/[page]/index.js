ffeef78f867e21409ef56bca63a61369fee4dde9
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-11-29 13:09:58 +0300

    Hotfix scripts base url

--- src/pages/[page]/index.js
+++ src/pages/[page]/index.js
@@ -7,6 +7,7 @@ import Page from '../../components/Page';
 
 import { get } from '../../getTakwimuPage';
 import Section from '../../components/Section';
+import config from '../../config';
 
 function P({ takwimu, wp }) {
   if (!wp) {
@@ -17,15 +18,15 @@ function P({ takwimu, wp }) {
       <Head>
         <link
           rel="stylesheet"
-          href="http://localhost:8080/wp-admin/load-styles.php?c=0&dir=ltr&load%5B%5D=dashicons,admin-bar,buttons,media-views,editor-buttons,wp-components,wp-block-editor,wp-nux,wp-editor,wp-block-library,wp-block-&load%5B%5D=library-theme,wp-edit-blocks,wp-edit-post,wp-format-library,common,forms,admin-menu,dashboard,list-tables,edit,revisions,media,t&load%5B%5D=hemes,about,nav-menus,wp-pointer,widgets,site-icon,l10n,wp-auth-check,wp-color-picker&ver=5.2.3"
+          href={`${config.WP_BACKEND_URL}//wp-admin/load-styles.php?c=0&dir=ltr&load%5B%5D=dashicons,admin-bar,buttons,media-views,editor-buttons,wp-components,wp-block-editor,wp-nux,wp-editor,wp-block-library,wp-block-&load%5B%5D=library-theme,wp-edit-blocks,wp-edit-post,wp-format-library,common,forms,admin-menu,dashboard,list-tables,edit,revisions,media,t&load%5B%5D=hemes,about,nav-menus,wp-pointer,widgets,site-icon,l10n,wp-auth-check,wp-color-picker`}
         />
         <link
           rel="stylesheet"
-          href="http://localhost:8080/wp-includes/js/mediaelement/wp-mediaelement.min.css?ver=5.2.3"
+          href={`${config.WP_BACKEND_URL}/wp-includes/js/mediaelement/wp-mediaelement.min.css`}
         />
         <script
           type="text/javascript"
-          src="http://localhost:8080/wp-content/themes/hurumap/assets/js/hurumap-iframe-handler.js"
+          src={`${config.WP_BACKEND_URL}//wp-content/themes/hurumap/assets/js/hurumap-iframe-handler.js`}
         />
       </Head>
       <Section>
