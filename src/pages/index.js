047e5faba7dc5374e1600b8593760299d454cc05
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-12-03 18:15:00 +0300

    Refactor remove hydration scripts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { getChartDefinitions, getSitePage } from '../getTakwimuPage';
+import Head from 'next/head';
+import { getSitePage } from '../getTakwimuPage';
 import FeaturedAnalysis from '../components/FeaturedAnalysis';
-import FeaturedData from '../components/FeaturedData';
 import Hero from '../components/Hero';
 import LatestNewsStories from '../components/LatestNewsStories';
 import MakingOfTakwimu from '../components/MakingOfTakwimu';
 import Page from '../components/Page';
 import WhatYouDoWithTakwimu from '../components/WhatYouCanDoWithTakwimu';
 import WhereToNext from '../components/Next';
+import config from '../config';
+import Section from '../components/Section';
 
-function Home({ chartDefinitions, indicatorId, latestMediumPosts, takwimu }) {
-  const { hurumap, flourish } = chartDefinitions;
-  /**
-   * Apply queryAlias
-   */
-  const charts = {
-    hurumap: hurumap.map((chart, i) => ({
-      ...chart,
-      visual: {
-        ...JSON.parse(chart.visual),
-        queryAlias: `v${i}`
-      },
-      stat: {
-        ...JSON.parse(chart.stat),
-        queryAlias: `v${i}`
-      }
-    })),
-    flourish
-  };
+function Home({ indicatorId, latestMediumPosts, takwimu }) {
   const {
     page: {
       rendered: featuredData,
@@ -40,16 +24,21 @@ function Home({ chartDefinitions, indicatorId, latestMediumPosts, takwimu }) {
 
   return (
     <Page takwimu={takwimu} indicatorId={indicatorId}>
+      <Head>
+        <script
+          src={`${config.WP_BACKEND_URL}/wp-content/themes/hurumap/micro-frontend/build/hurumap-ui-blocks.js`}
+        />
+      </Head>
       <Hero takwimu={takwimu} />
       <FeaturedAnalysis takwimu={takwimu} />
-      <FeaturedData charts={charts} takwimu={takwimu}>
+      <Section title="Featured Data">
         <div
           // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{
             __html: featuredData
           }}
         />
-      </FeaturedData>
+      </Section>
       <WhatYouDoWithTakwimu takwimu={takwimu} />
       <MakingOfTakwimu takwimu={takwimu} />
       <LatestNewsStories takwimu={takwimu} stories={latestMediumPosts} />
@@ -62,10 +51,6 @@ function Home({ chartDefinitions, indicatorId, latestMediumPosts, takwimu }) {
 }
 
 Home.propTypes = {
-  chartDefinitions: PropTypes.shape({
-    hurumap: PropTypes.arrayOf(PropTypes.shape({})),
-    flourish: PropTypes.arrayOf(PropTypes.shape({}))
-  }).isRequired,
   indicatorId: PropTypes.string,
   latestMediumPosts: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   takwimu: PropTypes.shape({
@@ -82,13 +67,11 @@ Home.defaultProps = {
 };
 
 Home.getInitialProps = async ({ query: { indicator: indicatorId } }) => {
-  const chartDefinitions = await getChartDefinitions();
   const res = await fetch('https://stories.hurumap.org/@takwimu_africa/latest');
   const latestMediumPosts = await res.json();
   const takwimu = await getSitePage('index');
 
   return {
-    chartDefinitions,
     indicatorId,
     latestMediumPosts,
     takwimu
