143b0f21390cfa2bcedee33fa5424dc629f9120d
khadija <khadijamahanga@y7mail.com>
2019-12-06 07:52:58 +0300

    install reactivesearch package + initial autosuggest component

---
+++ src/components/Autosuggest.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ReactiveBase } from '@appbaseio/reactivesearch';
+
+function Autosuggest() {
+  return (
+    <ReactiveBase
+    // app="carstore-dataset"
+    // credentials="4HWI27QmA:58c731f7-79ab-4f55-a590-7e15c7e36721"
+    >
+      {/* other components will go here. */}
+      <div>Hello ReactiveSearch!</div>
+    </ReactiveBase>
+  );
+}
+
+export default Autosuggest;
