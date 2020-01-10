f41e1f7b5f6f58604866592976101d0228665430
Clemence Kyara <clemence@codeforafrica.org>
2020-01-10 09:56:47 +0300

    Add robots confi for dev hosts as well as dev & prod content

--- src/config.js
+++ src/config.js
@@ -11,6 +11,17 @@ const config = {
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
       : 'https://takwimu.africa',
+  robots: {
+    devHosts: ['dev.takwimu.africa', 'now.sh', 'localhost:3000'],
+    dev: `
+User-agent: *
+Disallow: /
+    `,
+    prod: `
+User-agent: *
+Disallow:
+    `
+  },
   WP_BACKEND_URL,
   WP_HURUMAP_DATA_API: `${WP_BACKEND_URL}/wp-json/hurumap-data`,
   country: {},
