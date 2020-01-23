330614d38981dc604b8f0925422fee9240a4482d
KhadijaMahanga <khadijamahanga@y7mail.com>
2020-01-23 12:56:03 +0300

    Selection component from analysis + topic PR

--- src/components/CountryContent/Selection.js
+++ src/components/CountryContent/Selection.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PropTypes } from 'prop-types';
 
 import { FormControl, Input, MenuItem, Select } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { KeyboardArrowDown, KeyboardArrowUp } from '@material-ui/icons';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(({ breakpoints, palette, typography }) => ({
   root: {
     width: '100%',
     marginTop: '0.9375rem',
-    [theme.breakpoints.up('md')]: {
+    [breakpoints.up('md')]: {
       marginTop: 0,
       marginLeft: '0.703125rem',
       marginRight: '0.796875rem',
       width: '12.09375rem'
     },
-    [theme.breakpoints.up('lg')]: {
+    [breakpoints.up('lg')]: {
       marginTop: 0,
       marginLeft: '0.9375rem',
       marginRight: '1.0625rem',
@@ -30,23 +30,23 @@ const useStyles = makeStyles(theme => ({
     color: '#29a87c'
   },
   selectText: {
-    fontSize: theme.typography.body1.fontSize,
-    fontFamily: theme.typography.body1.fontFamily
+    fontSize: typography.body1.fontSize,
+    fontFamily: typography.body1.fontFamily
   },
   selectMenu: {
     borderRadius: '0.25rem',
     border: '1px solid rgba(151, 151, 151, 0.3)',
     padding: '0.875rem 2.0625rem 0.875rem 0.9375rem',
-    backgroundColor: theme.palette.background.default
+    backgroundColor: palette.background.default
   },
   menuPaper: {
-    backgroundColor: theme.palette.background.default
+    backgroundColor: palette.background.default
   },
   menuItem: {
-    fontSize: theme.typography.body1.fontSize,
-    fontFamily: theme.typography.body1.fontFamily,
+    fontSize: typography.body1.fontSize,
+    fontFamily: typography.body1.fontFamily,
     '&:selected': {
-      backgroundColor: theme.palette.background.default
+      backgroundColor: palette.background.default
     }
   }
 }));
@@ -65,64 +65,50 @@ SelectionIcon.propTypes = {
 
 const SelectionIconComponent = SelectionIcon;
 
-class Selection extends React.Component {
-  constructor(props) {
-    super(props);
+function Selection(props) {
+  const classes = useStyles(props);
+  const [open, setOpen] = useState(false);
+  const { onChange, items, value } = props;
 
-    this.state = { open: false };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(prevState => ({
-      open: !prevState.open
-    }));
-  }
-
-  render() {
-    const classes = useStyles();
-    const { onChange, items, value } = this.props;
-    const { open } = this.state;
-
-    return (
-      <FormControl variant="outlined" className={classes.root}>
-        <Select
-          value={value}
-          onChange={onChange}
-          classes={{
-            root: classes.selectText,
-            selectMenu: classes.selectMenu
-          }}
-          IconComponent={() => <SelectionIconComponent open={open} />}
-          onClick={this.handleClick}
-          input={<Input disableUnderline />}
-          MenuProps={{
-            classes: { paper: classes.menuPaper },
-            anchorOrigin: {
-              vertical: 'bottom',
-              horizontal: 'left'
-            },
-            transformOrigin: {
-              vertical: 'top',
-              horizontal: 'left'
-            },
-            getContentAnchorEl: null
-          }}
-        >
-          {items.map(item => (
-            <MenuItem
-              className={classes.menuItem}
-              key={item.value}
-              value={item.value}
-            >
-              {item.label}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    );
-  }
+  return (
+    <FormControl variant="outlined" className={classes.root}>
+      <Select
+        value={value}
+        onChange={onChange}
+        classes={{
+          root: classes.selectText,
+          selectMenu: classes.selectMenu
+        }}
+        IconComponent={() => <SelectionIconComponent open={open} />}
+        open={open}
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+        input={<Input disableUnderline />}
+        MenuProps={{
+          classes: { paper: classes.menuPaper },
+          anchorOrigin: {
+            vertical: 'bottom',
+            horizontal: 'left'
+          },
+          transformOrigin: {
+            vertical: 'top',
+            horizontal: 'left'
+          },
+          getContentAnchorEl: null
+        }}
+      >
+        {items.map(item => (
+          <MenuItem
+            className={classes.menuItem}
+            key={item.value}
+            value={item.value}
+          >
+            {item.label}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
 }
 
 Selection.propTypes = {
