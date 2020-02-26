a9badc1d34f7ca6ad6032f84113e00224ecc9348
khadija <khadijamahanga@y7mail.com>
2020-02-26 09:06:36 +0300

    select dropdown

--- src/components/Navigation/LanguageSelector.js
+++ src/components/Navigation/LanguageSelector.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 
-import { Menu, MenuItem, ButtonBase } from '@material-ui/core';
+import { MenuItem, Select } from '@material-ui/core';
 import languages from '../../languages';
 
 const useStyles = makeStyles(() => ({
@@ -27,34 +27,32 @@ const useStyles = makeStyles(() => ({
 function LanguageSelector({ defaultLanguage, availableLanguages, ...props }) {
   const classes = useStyles(props);
   const [selectedLang, setSelectedLang] = useState(defaultLanguage);
-  const [open, setOpen] = useState(false);
 
   return (
     <div className={classes.root}>
-      <ButtonBase onClick={() => setOpen(prevState => !prevState)}>
-        <img
-          src={languages[selectedLang.toUpperCase()]}
-          className={classes.flagImage}
-          alt="selected languge"
-        />
-      </ButtonBase>
-      <Menu open={open} className={classes.flagOptions}>
-        {availableLanguages
-          .filter(lang => lang !== selectedLang)
-          .map(lang => (
-            <MenuItem
-              key={lang}
-              className={classes.flagOption}
-              onClick={() => setSelectedLang(lang)}
-            >
-              <img
-                src={languages[lang.toUpperCase()]}
-                className={classes.flagImage}
-                alt="language option"
-              />
-            </MenuItem>
-          ))}
-      </Menu>
+      <Select
+        value={selectedLang}
+        renderValue={value => (
+          <img
+            src={languages[value.toUpperCase()]}
+            className={classes.flagImage}
+            alt="language option"
+          />
+        )}
+        IconComponent={() => null}
+        inputProps={{ disableUnderline: true }}
+        onChange={e => setSelectedLang(e.target.value)}
+      >
+        {availableLanguages.map(lang => (
+          <MenuItem key={lang} value={lang} className={classes.flagOption}>
+            <img
+              src={languages[lang.toUpperCase()]}
+              className={classes.flagImage}
+              alt="language option"
+            />
+          </MenuItem>
+        ))}
+      </Select>
     </div>
   );
 }
