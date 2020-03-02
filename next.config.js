749089accf2e8c190fb7c1d9a078ca4c16527e86
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-03-02 09:53:10 -0330

    Cleanup + use fix versions

--- next.config.js
+++ next.config.js
@@ -3,36 +3,32 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 });
 const withCSS = require('@zeit/next-css');
-const withTM = require('next-transpile-modules');
 const withImages = require('next-images');
 
 module.exports = withBundleAnalyzer(
   withCSS(
-    withImages(
-      withTM({
-        transpileModules: ['@hurumap-ui/core'],
-        target: 'serverless',
-        experimental: {
-          // https://github.com/zeit/next.js/issues/9081
-          async rewrites() {
-            return [
-              {
-                source: '/embed/:path*',
-                destination: '/embed'
-              }
-            ];
-          }
-        },
-        webpack(config, { webpack }) {
-          // ISSUE: https://github.com/bitinn/node-fetch/issues/412#issuecomment-379007792
-          // Should be ignore: https://github.com/zeit/next.js/issues/7621#issuecomment-504694519
-          config.plugins.push(
-            new webpack.IgnorePlugin(/^encoding$/, /node-fetch/)
-          );
-
-          return config;
+    withImages({
+      target: 'serverless',
+      experimental: {
+        // https://github.com/zeit/next.js/issues/9081
+        async rewrites() {
+          return [
+            {
+              source: '/embed/:path*',
+              destination: '/embed'
+            }
+          ];
         }
-      })
-    )
+      },
+      webpack(config, { webpack }) {
+        // ISSUE: https://github.com/bitinn/node-fetch/issues/412#issuecomment-379007792
+        // Should be ignore: https://github.com/zeit/next.js/issues/7621#issuecomment-504694519
+        config.plugins.push(
+          new webpack.IgnorePlugin(/^encoding$/, /node-fetch/)
+        );
+
+        return config;
+      }
+    })
   )
 );
