82da22872b87c69d4aca3360b2eb9c6162c374e5
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-03-09 06:28:11 -0230

    Use regex

--- babel.config.js
+++ babel.config.js
@@ -4,24 +4,24 @@ module.exports = {
     [
       'transform-imports',
       {
-        '@material-ui/core': {
-          transform: importName =>
-            importName.includes('style')
-              ? `@material-ui/styled/${importName}`
-              : `@material-ui/core/${importName}`,
-          // Whether or not to throw when an import is encountered which would cause the entire module to be imported.
-          preventFullImport: true
-        },
         '@material-ui/core/styles': {
           // eslint-disable-next-line no-template-curly-in-string
           transform: '@material-ui/styles/${member}',
           // Whether or not to throw when an import is encountered which would cause the entire module to be imported.
           preventFullImport: true
         },
+        '@material-ui/core(?!/)': {
+          transform: importName =>
+            importName.toLowerCase().includes('style')
+              ? `@material-ui/styles/${importName}`
+              : `@material-ui/core/${importName}`,
+          // Whether or not to throw when an import is encountered which would cause the entire module to be imported.
+          preventFullImport: true
+        },
         '@hurumap-ui/charts': {
           // eslint-disable-next-line no-template-curly-in-string
           transform: importName =>
-            importName.includes('chart')
+            importName.toLowerCase().includes('chart')
               ? `@hurumap-ui/charts/${importName}`
               : `@hurumap-ui/core/${importName}`,
           // Whether or not to throw when an import is encountered which would cause the entire module to be imported.
