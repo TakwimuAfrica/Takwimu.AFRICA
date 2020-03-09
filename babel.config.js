e6708a55aa8f8ee139488585f34a0341bdf876bf
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-03-09 05:52:59 -0230

    Format babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,30 +1,36 @@
-{
-  "presets": ["next/babel"],
-  "plugins": [
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [
     [
-      "transform-imports",
+      'transform-imports',
       {
-        "@material-ui/core": {
-          "transform": importName => importName.includes("style") ? `@material-ui/styled/${importName}` : `@material-ui/core/${importName}`,
+        '@material-ui/core': {
+          transform: importName =>
+            importName.includes('style')
+              ? `@material-ui/styled/${importName}`
+              : `@material-ui/core/${importName}`,
           // Whether or not to throw when an import is encountered which would cause the entire module to be imported.
-          "preventFullImport": true
+          preventFullImport: true
         },
-        "@material-ui/core/styles": {
-          "transform": "@material-ui/styles/${member}",
+        '@material-ui/core/styles': {
+          // eslint-disable-next-line no-template-curly-in-string
+          transform: '@material-ui/styles/${member}',
           // Whether or not to throw when an import is encountered which would cause the entire module to be imported.
-          "preventFullImport": true
+          preventFullImport: true
         },
-        "@hurumap-ui/charts": {
-          "transform": "@hurumap-ui/charts/${member}",
+        '@hurumap-ui/charts': {
+          // eslint-disable-next-line no-template-curly-in-string
+          transform: '@hurumap-ui/charts/${member}',
           // Whether or not to throw when an import is encountered which would cause the entire module to be imported.
-          "preventFullImport": true
+          preventFullImport: true
         },
-        "@hurumap-ui/content": {
-          "transform": "@hurumap-ui/content/${member}",
+        '@hurumap-ui/content': {
+          // eslint-disable-next-line no-template-curly-in-string
+          transform: '@hurumap-ui/content/${member}',
           // Whether or not to throw when an import is encountered which would cause the entire module to be imported.
-          "preventFullImport": true
+          preventFullImport: true
         }
       }
     ]
   ]
-}
+};
