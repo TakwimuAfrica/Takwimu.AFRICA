20f48008e705ca1b9aba71705365c89d637ff328
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-03-09 05:55:02 -0230

    HURUmap charts import from core

--- babel.config.js
+++ babel.config.js
@@ -20,7 +20,10 @@ module.exports = {
         },
         '@hurumap-ui/charts': {
           // eslint-disable-next-line no-template-curly-in-string
-          transform: '@hurumap-ui/charts/${member}',
+          transform: importName =>
+            importName.includes('chart')
+              ? `@hurumap-ui/charts/${importName}`
+              : `@hurumap-ui/core/${importName}`,
           // Whether or not to throw when an import is encountered which would cause the entire module to be imported.
           preventFullImport: true
         },
