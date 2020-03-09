6de4a2275f845a66be24f7373746d22e95ab97f4
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-03-09 05:47:01 -0230

    Material ui styles

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,12 @@
       "transform-imports",
       {
         "@material-ui/core": {
-          "transform": "@material-ui/core/${member}",
+          "transform": importName => importName.includes("style") ? `@material-ui/styled/${importName}` : `@material-ui/core/${importName}`,
+          // Whether or not to throw when an import is encountered which would cause the entire module to be imported.
+          "preventFullImport": true
+        },
+        "@material-ui/core/styles": {
+          "transform": "@material-ui/styles/${member}",
           // Whether or not to throw when an import is encountered which would cause the entire module to be imported.
           "preventFullImport": true
         },
