3c66e03d02cd874add2a2b7dfe11778a812d51d2
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-03-10 04:48:13 -0230

    Transform hurumap-ui

--- babel.config.js
+++ babel.config.js
@@ -9,13 +9,18 @@ module.exports = {
           transform: '@material-ui/core/${member}',
           preventFullImport: true
         },
-        '@hurumap-ui/charts': {
+        '@hurumap-ui/core': {
           // eslint-disable-next-line no-template-curly-in-string
           transform: importName =>
             importName.toLowerCase().includes('chart')
               ? `@hurumap-ui/charts/${importName}`
               : `@hurumap-ui/core/${importName}`,
           // Whether or not to throw when an import is encountered which would cause the entire module to be imported.
+          preventFullImport: true
+        },
+        '@hurumap-ui/charts': {
+          // eslint-disable-next-line no-template-curly-in-string
+          transform: '@hurumap-ui/charts/${member}',
           preventFullImport: true
         },
         '@hurumap-ui/content': {
