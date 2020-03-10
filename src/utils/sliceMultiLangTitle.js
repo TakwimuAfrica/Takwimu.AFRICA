7139273f2508abfe096cf68bfe7f0fccd87e2919
KhadijaMahanga <khadijamahanga@y7mail.com>
2020-03-10 17:46:13 +0300

    fix slice method

--- src/utils/sliceMultiLangTitle.js
+++ src/utils/sliceMultiLangTitle.js
@@ -1,7 +1,7 @@
 export default function sliceMultiLangTitle(title, lang) {
   const searchTerm = `[:${lang}]`;
-  const startIndex = title.indexOf(searchTerm) + searchTerm.length;
-  const endIndex = title.indexOf('[:', startIndex);
+  const startIndex = title.indexOf(searchTerm);
+  const endIndex = title.indexOf('[:', startIndex + searchTerm.length);
 
-  return title.slice(startIndex, endIndex + 1);
+  return title.slice(startIndex, endIndex);
 }
