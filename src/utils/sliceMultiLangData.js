ce4855237f1d3f4062e22be2e415a6ea125a7372
khadija <khadijamahanga@y7mail.com>
2020-03-11 12:52:33 +0300

    default lang term

--- src/utils/sliceMultiLangData.js
+++ src/utils/sliceMultiLangData.js
@@ -1,8 +1,12 @@
 export default function sliceMultiLangData(title, lang) {
-  const searchTerm = `[:${lang}]`;
-  const langIndex = title.indexOf(searchTerm);
+  const langTerm = `[:${lang}]`;
+  const defaultLangTerm = '[:en]';
+  const langIndex = title.includes(langTerm)
+    ? title.indexOf(langTerm)
+    : title.indexOf(defaultLangTerm);
+
   if (langIndex !== -1) {
-    const startIndex = langIndex + searchTerm.length;
+    const startIndex = langIndex + langTerm.length;
     const endIndex = title.indexOf('[:', startIndex);
 
     return title.slice(startIndex, endIndex);
