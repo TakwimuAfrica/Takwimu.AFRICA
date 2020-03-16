5d7911d791f8f80899109f024d4aac2e54fdad4d
Clemence Kyara <clemence@codeforafrica.org>
2020-03-16 12:18:28 +0300

    Update countrify to use config.language

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,12 +1,32 @@
 /* eslint-disable import/prefer-default-export */
+import config from '../config';
 
-export const countrify = (title, country, countries, conj = '&rsquo;s ') => {
+const DEFAULT_CONJ = ' ';
+
+export const countrify = (
+  title,
+  country,
+  countries = config.countries,
+  conj,
+  language = config.language
+) => {
   const foundCountry = countries.find(c => c.slug === country.slug);
   if (
     foundCountry &&
     !title.toLowerCase().startsWith(foundCountry.short_name.toLowerCase())
   ) {
-    return `${foundCountry.short_name}${conj}${title}`;
+    let langConj = conj;
+    if (!langConj) {
+      switch (language) {
+        case 'en':
+          langConj = '&rsquo;s ';
+          break;
+        default:
+          langConj = DEFAULT_CONJ;
+          break;
+      }
+    }
+    return `${foundCountry.short_name}${langConj}${title}`;
   }
   return title;
 };
