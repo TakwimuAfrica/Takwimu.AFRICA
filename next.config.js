e2e0d2c6bc2c50cfbb1b76312530e57ef2a77cd4
Clemence Kyara <clemence@codeforafrica.org>
2020-04-10 00:46:27 +0300

    Only process images from leaflet.css

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const withImages = require('next-images');
 module.exports = withBundleAnalyzer(
   withCSS(
     withImages({
-      webpack(config, { isServer, webpack }) {
+      webpack(config, { webpack }) {
         // ISSUE: https://github.com/bitinn/node-fetch/issues/412#issuecomment-379007792
         // Should be ignore: https://github.com/zeit/next.js/issues/7621#issuecomment-504694519
         config.plugins.push(
@@ -17,18 +17,11 @@ module.exports = withBundleAnalyzer(
 
         // support font & image imports inside css files
         config.module.rules.push({
-          test: /\.(woff|woff2|eot|ttf|otf|png|gif|ico|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          // leaflet.css loads these images which breaks default webpack
+          test: /(layers|layers-2x|marker-icon)\.png$/,
           use: [
             {
-              loader: require.resolve('file-loader'),
-              options: {
-                name: '[name]-[hash:8].[ext]',
-                publicPath: url => {
-                  return `/_next/static/css/${url}`;
-                },
-                outputPath: `${isServer ? '../' : ''}static/css/`,
-                esModule: false
-              }
+              loader: require.resolve('url-loader')
             }
           ]
         });
