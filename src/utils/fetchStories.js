844c27e5e434adeba2d0763c4cbedfba7c789941
Clemence Kyara <clemence@codeforafrica.org>
2020-06-11 17:53:57 +0300

    Use corsio to fetch stories

---
+++ src/utils/fetchStories.js
@@ -0,0 +1,21 @@
+import config from '../config';
+
+export default async function fetchStories() {
+  const urlJson = `https://corsio.devops.codeforafrica.org/?${config.stories.url}?format=json`;
+  const response = await fetch(urlJson);
+  const jsonClean = await (await response.text()).replace(
+    '])}while(1);</x>',
+    ''
+  );
+  const json = await JSON.parse(jsonClean);
+  const reportStreamItems = await json.payload.streamItems;
+  // console.log('BOOM', { json, reportStreamItems });
+  const reports = await reportStreamItems.map(
+    reportStreamItem =>
+      reportStreamItem.postPreview &&
+      json.payload.references.Post[reportStreamItem.postPreview.postId]
+  );
+
+  // Remove null stories
+  return reports.filter(story => story);
+}
